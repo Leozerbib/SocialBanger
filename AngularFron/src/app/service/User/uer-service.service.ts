4f63ab65fde5f9714bf9ecf00a83ec4baed0e17b
Leozerbib <116356061+Leozerbib@users.noreply.github.com>
2024-05-21 18:49:41 +0200

    ll

--- AngularFron/src/app/service/User/uer-service.service.ts
+++ AngularFron/src/app/service/User/uer-service.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { environment } from '../../../environment/environment';
+import { environment } from '../../../environments/environment';
 import { User } from '../../model/User/user.model';
 
 @Injectable({ providedIn: 'root' })
